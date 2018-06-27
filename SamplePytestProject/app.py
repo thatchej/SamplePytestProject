9e31a858ffacb17239059befa70fe345d84bdfee
Tom Bell <tjbell@gmail.com>
2018-06-27 13:43:53 -0600

    Answers

--- SamplePytestProject/app.py
+++ SamplePytestProject/app.py
@@ -1,11 +1,32 @@
+# Range divisible by 7 but are not a multiple of 5
 def div(min_value, max_value):
-    pass
+    my_list = []
+    for i in range(min_value, max_value):
+        if (i % 7 == 0) and (i % 5 != 0):
+            my_list.append(i)
+    return my_list
 
-def fact(x):
-    pass
+# Factorial
+def fact(n):
+    if n == 0:
+        return 1
+    return n * fact(n -1)
 
+# Flatten List
 def flatten(lst):
-    pass
+    return sum(([x] if not isinstance(x, list) else flatten(x) for x in lst), [])
 
+
+# Greatest Common Divisor
 def gcd(my_list):
-    pass
+    while len(my_list) > 1:
+        a = my_list[len(my_list) - 2]
+        b = my_list[len(my_list) - 1]
+        my_list = my_list[:len(my_list) - 2]
+
+        while a:
+            a, b = b % a, a
+
+        my_list.append(b)
+
+    return abs(b)
